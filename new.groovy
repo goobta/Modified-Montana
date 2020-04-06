b63532d80c73f766c15714a21d40afd5b16b1f33
Ankur Gupta <rukna1000@gmail.com>
2020-04-06 18:08:57 -0400

    Got the cover working

--- new.groovy
+++ new.groovy
@@ -1,8 +1,17 @@
 // Load an STL file from a git repo
 // Loading a local file also works here
-File servoFile = ScriptingEngine.fileFromGit(
+File maskFile = ScriptingEngine.fileFromGit(
 	"https://github.com/agupta231/Modified-Montana.git",
 	"MontanaMasks.stl");
 // Load the .CSG from the disk and cache it in memory
-CSG servo  = Vitamins.get(servoFile);
-return servo;
+CSG mask = Vitamins.get(maskFile);
+
+mask = mask.rotx(90)
+		 .movez(-7);
+
+List<Polygon> polys = Slice.slice(mask, new Transform(), 0);
+Polygon coverOutline = polys[0];
+
+CSG cover = Extrude.polygons(coverOutline, coverOutline.transformed(new Transform().movez(5.0)));
+
+return [mask, cover];
